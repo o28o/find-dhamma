fd1a66b9ec3678c321ea65ca9876f3b5e405b0db
o server <o28o@mail.ru>
2022-10-05 20:59:46 +0300

    update from server

--- ru.php
+++ ru.php
@@ -86,14 +86,15 @@ $pattern = $language = $arg = "";
       <!-- <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="/">Main</a></li> 
 	  nav-link -->
 	  
+	  
 <li class="nav-item mx-0 mx-lg-2"><a class="nav-link py-3 px-0 px-lg-0 rounded" href="https://find.dhamma.gift/sc/">SC Лайт</a></li>
-<li class="nav-item mx-0 mx-lg-2"><a class="nav-link py-3 px-0 px-lg-0 rounded" href="/list.php?lang=pali">История Поиска</a></li>
-<li class="nav-item mx-0 mx-lg-2"><a class="nav-link py-3 px-0 px-lg-0 rounded" href="#help">Помощь</a></li>
+<li class="nav-item mx-0 mx-lg-2"><a class="nav-link my-3 py-10 px-0 px-lg-0 rounded" href="/list.php?lang=pali">История Поиска</a></li>
+<li class="nav-item mx-0 mx-lg-2"><a class="nav-link my-3 py-10 px-0 px-lg-0 rounded" href="#help">Помощь</a></li>
 <li class="nav-item mx-0 mx-lg-2"><a class="nav-link py-3 px-0 px-lg-0 rounded" href="#project">О Нас</a></li>             
 <li class="nav-item mx-0 mx-lg-2"><a class="nav-link py-3 px-0 px-lg-0 rounded" href="#links">Полезное</a></li>
 <li class="nav-item mx-0 mx-lg-2"><a class="nav-link py-3 px-0 px-lg-0 rounded" href="#contacts">Контакты</a></li>
-<li class="nav-item mx-0 mx-lg-2"><p><a class="py-1 px-0 px-lg-1 rounded link-light text-decoration-none" href="/">En</a> 
-									<a class="link-light py-1 px-0 px-lg-1 rounded" href="/ru.php">Ru</a></p></li>	
+<li class="nav-item mx-0 mx-lg-2"><p><a class="py-3 px-0 px-lg-1 rounded link-light text-decoration-none" href="/">En</a> 
+									<a class="py-3 px-0 px-lg-1 rounded link-light" href="/ru.php">Ru</a></p></li>	
                     </ul>
                 </div>
             </div>
